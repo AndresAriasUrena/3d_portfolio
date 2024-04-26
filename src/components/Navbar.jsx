5cf6cca2628b32727effe0433d637fc6e4d558f9
Andres Arias <andresarias@Andress-MacBook-Pro.local>
2024-04-26 04:13:43 -0600

    logo up to date

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,11 +23,11 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+          <img src={logo} alt="logo" className="w-32 h-9 object-contain" />
+          {/* <p className="text-white text-[18px] font-bold cursor-pointer flex">
             Andres &nbsp;
             <span className="sm:block hidden">| MDW</span>
-          </p>
+          </p> */}
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((link) => (
